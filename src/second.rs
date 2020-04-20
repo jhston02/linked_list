f3cd5867729db0f86c840eea135e7393d0ed5c47
John Stone <coleslaw.john@gmail.com>
2020-04-20 19:18:03 -0400

    first iterator

--- src/second.rs
+++ src/second.rs
@@ -11,6 +11,8 @@ pub struct List<T> {
     head: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -38,6 +40,18 @@ impl<T> List<T> {
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
+    }
+
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -90,5 +104,20 @@ mod test {
         list.peek_mut().map(|value| {
             *value = 42;
         });
+    }
+
+    #[test]
+    fn iterator() {
+        let mut list = super::List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
