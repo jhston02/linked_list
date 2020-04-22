bbd2e3e27b7b10256a8f31492ce0a01ef75b361c
John Stone <coleslaw.john@gmail.com>
2020-04-21 22:17:05 -0400

    borrow immutable iterator

--- src/second.rs
+++ src/second.rs
@@ -13,6 +13,10 @@ pub struct List<T> {
 
 pub struct IntoIter<T>(List<T>);
 
+pub struct Iter<'a,T> {
+    next: Option<&'a Node<T>>
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -45,6 +49,10 @@ impl<T> List<T> {
     fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {next: self.head.as_ref().map(|node| &**node)}
+    }
 }
 
 impl<T> Iterator for IntoIter<T> {
@@ -52,6 +60,17 @@ impl<T> Iterator for IntoIter<T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
     }
 }
 
